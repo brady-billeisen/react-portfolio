9b1847b7d86518824e1d155ed0260fdd9faebdb5
brady <bradybill36@gmail.com>
2023-09-15 15:59:10 -0500

    got navigation fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Navigation from './components/Navigation'
+import Header from './components/Header';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 function App() {
   return (
     <Router>
-      <Navigation />
+      <Header />
       <Routes>
         <Route element={<Home />} path='/'/>
         <Route element={<About />} path='/about'/>
+        <Route element={<Portfolio />} path='/portfolio'/>
+        <Route element={<Resume />} path='/resume'/>
         <Route element={<Contact />} path='/contact'/>
       </Routes>
     </Router>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavbarBrand } from 'react-bootstrap';
 
 function Navigation() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-            <Navbar.Brand href="#home">Brady Billeisen</Navbar.Brand>
+            <NavbarBrand>
+                <Link to='/'>
+                    <h1 className="text-dark fs-3">Brady Billeisen</h1>
+                </Link>
+            </NavbarBrand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Link to='/' style={{ margin: "0 4px" }}>Home</Link>
                     <Link to='/about' style={{ margin: "0 4px" }}>About</Link>
+                    <Link to='/portfolio' style={{ margin: "0 4px" }}>Portfolio</Link>
+                    <Link to='/resume' style={{ margin: "0 4px" }}>Resume</Link>
                     <Link to='/contact' style={{ margin: "0 4px" }}>Contact</Link>
                 </Nav>
             </Navbar.Collapse>
