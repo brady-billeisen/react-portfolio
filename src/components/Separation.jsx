423592e1bc972715f38605d285157450aab94dfe
brady <bradybill36@gmail.com>
2023-09-18 18:56:22 -0500

    made some small design changes

--- src/components/Separation.jsx
+++ src/components/Separation.jsx
@@ -7,16 +7,16 @@ function Separation() {
   return (
     <Container id="footer">
         <Nav className="p-2">
-          <Container>
-          </Container>
           <Container className="d-flex justify-content-start align-items-center">
             <h2 className="d-flex justify-content-start text-light">Portfolio</h2>
-            <Link to='https://github.com/brady-billeisen'>
-              <img src={githubLogo} alt="GitHub Logo" className="ms-2 me-2"/>
-            </Link>
-            <Link to='https://www.linkedin.com/in/bradybilleisen/'>
-              <img src={linkedinLogo} alt="LinkedIn Logo" className="ms-2 me-2" />
-            </Link>
+            <Container className="d-flex justify-content-end">
+              <Link to='https://github.com/brady-billeisen'>
+                <img src={githubLogo} alt="GitHub Logo" className="ms-2 me-2"/>
+              </Link>
+              <Link to='https://www.linkedin.com/in/bradybilleisen/'>
+                <img src={linkedinLogo} alt="LinkedIn Logo" className="ms-2 me-2" />
+              </Link>
+            </Container>
           </Container>
         </Nav>
     </Container>
