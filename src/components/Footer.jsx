a974742c7547652b2abe3f49458d10b03df22fe4
brady <bradybill36@gmail.com>
2023-09-18 22:36:06 -0500

    fixed the width issue, as well as a couple other issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
                 <Link to='/resume' style={{ margin: "0 4px" }} id='footer-link'>Resume</Link>
                 <Link to='/contact' style={{ margin: "0 4px" }} id='footer-link'>Contact</Link>
             </Container>
-            <Container>
+            <Container className='pb-5'>
                 <p className='d-flex justify-content-center text-light m-4'>This is the whole point of technology.  It creates an appetite for immortality on the one hand.  It threatens universal extinction on the other. Technology is lust removed from nature. - Don DeLillo (Author)</p>
             </Container>
         </Container>
