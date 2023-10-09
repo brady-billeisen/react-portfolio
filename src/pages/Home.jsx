5068fa7b94e375bd18f8842ecb63f48f34b1d651
brady <bradybill36@gmail.com>
2023-10-09 17:36:42 -0400

    made some small changes to the transitions, and cleaned up my code a bit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,21 +11,28 @@ function Home() {
           <img src={granularGarden} id='carousel-img' />
           <Carousel.Caption id='home-captions'>
             <h3>Granular Garden</h3>
-            <p className='ms-4 me-4'>A music streaming platform made using the MVC framework, Express.js, Handlebars, and more technologies!</p>
+            <p className='ms-4 me-4'>
+              A music streaming platform made using the MVC framework, Express.js, Handlebars, and more technologies!
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img src={esoClassGuides} id='carousel-img' />
           <Carousel.Caption id='home-captions'>
             <h3>ESO ClassGuides</h3>
-            <p className='ms-4 me-4'>An app tailored specifically to help players of the game Elder Scrolls Online by pulling the most popular posts relevant to your class choice, from Reddit & Youtube!</p>
+            <p className='ms-4 me-4'>
+              An app tailored specifically to help players of the game Elder Scrolls Online by pulling the most popular posts 
+              relevant to your class choice, from Reddit & Youtube!
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img src={JATE} id='carousel-img' />
           <Carousel.Caption id='home-captions'>
             <h3>JATE (Just Another Text Editor)</h3>
-            <p className='ms-4 me-4'>A progressive web app text editor made for working with JavaScript!</p>
+            <p className='ms-4 me-4'>
+              A progressive web app text editor made for working with JavaScript!
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
